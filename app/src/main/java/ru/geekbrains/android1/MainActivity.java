f3dba808f03b6540c7c1dfb6eadfe3fd8ea9bd5a
SergeyTsemerov <sergeytsemerov@gmail.com>
2021-05-27 16:37:31 +0300

    Activity changers and scroll view

--- app/src/main/java/ru/geekbrains/android1/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/MainActivity.java
@@ -3,12 +3,21 @@ package ru.geekbrains.android1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void switchLayout(View view) {
+        setContentView(R.layout.activity_another_tasks);
+    }
+
+    public void switchLayout1(View view) {
         setContentView(R.layout.activity_main);
     }
 }
